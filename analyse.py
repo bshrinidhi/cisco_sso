f477b2ae448a9440ca6527d1af984e772b01044d
Sarvesh Malagi (samalagi) <samalagi@cisco.com>
2018-01-10 20:40:26 +0530

    Added analyse.py

--- analyse.py
+++ analyse.py
@@ -1,1 +1,17 @@
+import json
+
+data = json.load(open('C:/Users/samalagi/Assignment/cisco_sso/data.json'))
+network = data['network']
+devices = network['devices']
+
+for device in devices:
+    for interface in device['interfaces']:
+        broadcast = interface['broadcasts']
+        packet = interface['input_packets']
+        if packet != 0:
+            interface['broadcast_percent'] = broadcast/packet
+
+print(data)
+with open('data_a.json', 'w') as fp:
+    json.dump(data, fp, indent=4)
 
