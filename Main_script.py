2dd769399fc0e81063150ad51b821c6c8988f254
bshrinidhi <shrinb@cisco.com>
2018-01-10 17:02:12 +0530

    Minor update..Commented print statements

--- Main_script.py
+++ Main_script.py
@@ -4,12 +4,12 @@ import json
 def assessment():
     with open('data.json') as json_data:
         data=json.load(json_data)
-        print(data)
+        #print(data)
     analyse.json_analyse(data)
-    with open('data_a') as json_data:
+    with open('data_a.json') as json_data:
         data_a=json.load(json_data)
     violate.json_violate(data_a)
-    with open('data_v') as json_data:
+    with open('data_v.json') as json_data:
         data_v=json.load(json_data)
-        print(data_v)
+        #print(data_v)
 assessment()
